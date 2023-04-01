cbdf07d0e46736744274a0e8e6b4eae0985f27c1
DawidKaczy <116282752+DawidKaczy@users.noreply.github.com>
2023-04-01 15:33:49 +0200

    Kolos

--- main.py
+++ main.py
@@ -1,160 +1,66 @@
-a ='napis'
-print(a)
-
-b = 5
-c = 5.5
-print(b, c)
-
-d = 5+5j
-print(d)
-
-e = b + c
-print(e)
-
-f = b**2
-print(f)
-# lub (potegi)
-g = pow(5, 2)
-print(g)
-
-b +=2
-print(b)
-
-listy = ['a','b,',4,5,6,[1,2,3],6]
-print(listy)
-listy.append(4)
-print(listy)
-print(listy[1])
-
-#dodat elementy na wybranej miejscu
-#usunecie elementu z listy danej pozycji
-#usuniecie elemntu z listy o danej wartosci
-#usuniecie elementy listy
-#zrobic sortowanie
-
-listy2 = ['ok1','ok2','ok3','ok4']
-print(listy2)
-listy2.insert(2,6) #po index2 dodajemy 6
-print(listy2)
-
-#Usuń 4 element:
-listy2.pop(3)
-print(listy2)
-
-#Usuwanie po nazwie
-listy2.remove('ok1')
-print(listy2)
-
-numery = [1,2,4,3,2,7,8,4,2]
-numery.sort() #sortowanie rosnaco
-print(numery)
-
-numery.sort(reverse = True) #malejaco
-print(numery)
-
-slownik = {'a': 2,1 : 2,2:'ab'}
-print(slownik)
-print(slownik[2])
-slownik['klucz']='wartosc'
-print(slownik)
-slownik.pop('klucz')
-print(slownik)
-
-
-napis = 'a={}, b={}'
-print(napis.format(12,12))
-#lub
-print('a={}, b={}'.format(12,12))
-
-#if warunek
-   #instrukcja1
-   #instrukcja2
-#elif warunek2
-   #instrukcja1
-   #instrukcja2
-#else
-   #instrukcje
-
-
-# a = input('podaj a')
-# b = input('podaj b')
-# print(a)
-# print(b)
-# print(type(a))
-# print(type(b))
-# a = int(a)
-# b = int(b)
-# print(type(a))
-# print(type(b))
+import math
+# zad1
+# try:
+#     a = int(input("Podaj a: "))
+#     b = int(input("Podaj b: "))
 #
-# if a>b:
-#     print(a)
-# elif a<b:
-#     print(b)
-# else:
-#     print('a rowne b')
+#     c = (a**2)+(a*b)+(b**2)
 #
-# if a==b:
-#     print("sa rowne")
-# else:
-#     print('nie sa rowne')
-
-
-
-# a = input('podaj a')
-# b = input('podaj b')
-# c = input('podaj c')
-# d = input('podaj d')
+#     plik =open("zadanie1.txt", "w")
+#     plik.write(str(c))
+#     plik.close()
+#     print(c)
 #
-# a = int(a)
-# b = int(b)
-# c = int(c)
-# d = int(d)
-#
-# if a>b & c>d:
-#     print('a wieksze od b i c wieksze od d')
-# else:
-#     print('a jest mniejsze od b lub c kest wieksze od d')
+# except:
+#     ValueError:print("Wprowadzona wartość nie jest liczbą całkowitą")
 
-for x in range(1, 6, 1):
-    print(x)
+#zad 2
+def tworzenieListy(lista1, lista2):
+    listaWynik = []
+    for i in range(min(len(lista1), len(lista2))):
+        listaWynik.append(lista1[i] + lista2[i])
+    return listaWynik
+
+print(tworzenieListy([1,2,3,4],[2,2,2,2,2]))
+
+#zad 3
+plikteskt = open("tekst.txt","r",encoding="utf8")
+znaki100 = plikteskt.read(99)
+znaki35 = plikteskt.read(35)
+print(znaki35)
+
+wynik = 0
+for i in znaki35:
+    if i.isupper():  #lub dla małych islower
+        print(i)
+        wynik = wynik + 1
+
+if(znaki35 == 0):
+    print("Brak dużych liter")
 else:
-    print('koniec petli for')
+    print("Ilość dużych znaków:",wynik)
 
-for x in listy:
-    print(x)
+#zad4
+lista = [2, 7, 13, 4, 8, 6, 10]
+a = 5
+listawynik = []
+for i in range(len(lista)):
+    if(lista[i] > a):
+        listawynik.append(lista[i])
+print(listawynik)
 
+#zad4 python comprehension
+liczby = [2, 7, 13, 4, 8, 6, 10]
+b = 5
+nowa_lista = [liczba for liczba in liczby if liczba > a]
+print(nowa_lista)
 
+#zad5
+e = math.e
+pi = math.pi
+cos39 = math.cos(39)
 
-for i in range(0, len(listy),1):
-    print(listy[i])
+wynik = ((e**3 + cos39**2)**(1/5)) + ((2/7)**3) + pi
+wynik = round(wynik, 2)
 
-#while
-
-# licznik = 0
-# while licznik !=len(listy):
-#     print(listy[licznik])
-#     licznik += 1
-
-# liczby2 = [3,4,5,1,7,8]
-# a = int(input('podaj a'))
-# licznik = 0
-# while licznik != len(liczby2):
-#     if a - liczby2[licznik] == 0:
-#         print('{} - {} = 0'.format(a, liczby2[licznik]))
-#         break
-#         licznik +=1
-
-liczby2 = [1,2,2,2,3,4]
-licznik = 0
-while licznik != len(liczby2):
-    if liczby2[licznik] == 2:
-        liczby2.pop(licznik)
-    else:
-        licznik += 1
-print(liczby2)
-
-
-
-
-
+print(wynik)
